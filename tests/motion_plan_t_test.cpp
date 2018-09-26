d15783df333bb8dd8e61cb101cd293a939283382
pantadeusz <none@none.com>
2018-09-26 18:21:58 +0200

    additional test for motion_plan_t

---
+++ tests/motion_plan_t_test.cpp
@@ -0,0 +1,160 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+
+#include <configuration_t_json.hpp>
+#include <motion_plan_t.hpp>
+#include <vector>
+
+using namespace raspigcd;
+
+TEST_CASE("Steps calculated by motion_plan_t", "[motion_plan_t]")
+{
+    SECTION("long compare two methods of path generation")
+    {
+        std::vector<executor_command_t> planA;
+        std::vector<executor_command_t> planB;
+        configuration_t cfg;
+        cfg.load_defaults();
+        std::cout << cfg << std::endl;
+        {
+            motion_plan_t mp(cfg);
+            mp.gotoxy(distance_t{5.0, 0.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{5.0, -2.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{5.0, -5.0, 0.0, 0.0}, 5.0)
+                .gotoxy(distance_t{0.0, -5.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{0.0, 0.0, 0.0, 0.0}, 20.0);
+            planA = mp.get_motion_plan();
+        }
+        {
+            std::vector<executor_command_t> fragment;
+            motion_plan_t mp(cfg);
+            mp.gotoxy(distance_t{5.0, 0.0, 0.0, 0.0}, 20.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+
+            mp.gotoxy(distance_t{5.0, -2.0, 0.0, 0.0}, 20.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+
+            mp.gotoxy(distance_t{5.0, -5.0, 0.0, 0.0}, 5.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+
+            mp.gotoxy(distance_t{0.0, -5.0, 0.0, 0.0}, 20.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+
+            mp.gotoxy(distance_t{0.0, 0.0, 0.0, 0.0}, 20.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+        }
+
+
+        //std::vector<executor_command_t> commands;
+        //steps_t steps = executor_t::commands_to_steps(commands);
+        steps_t planAsteps = executor_t::commands_to_steps(planA);
+        steps_t planBsteps = executor_t::commands_to_steps(planB);
+
+        REQUIRE(planAsteps == planBsteps);
+    }
+
+    SECTION("short compare two methods of path generation")
+    {
+        std::vector<executor_command_t> planA;
+        std::vector<executor_command_t> planB;
+        configuration_t cfg;
+        cfg.load_defaults();
+        std::cout << cfg << std::endl;
+        {
+            motion_plan_t mp(cfg);
+            mp.gotoxy(distance_t{5.0, 0.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{5.0, -2.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{5.0, -5.0, 0.0, 0.0}, 5.0);
+            planA = mp.get_motion_plan();
+        }
+        {
+            std::vector<executor_command_t> fragment;
+            motion_plan_t mp(cfg);
+            mp.gotoxy(distance_t{5.0, 0.0, 0.0, 0.0}, 20.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+
+            mp.gotoxy(distance_t{5.0, -2.0, 0.0, 0.0}, 20.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+
+            mp.gotoxy(distance_t{5.0, -5.0, 0.0, 0.0}, 5.0);
+            fragment = mp.get_motion_plan();
+            planB.insert(planB.end(), fragment.begin(), fragment.end());
+            mp.clear_motion_fragments_buffer();
+        }
+
+
+        //std::vector<executor_command_t> commands;
+        //steps_t steps = executor_t::commands_to_steps(commands);
+        steps_t planAsteps = executor_t::commands_to_steps(planA);
+        steps_t planBsteps = executor_t::commands_to_steps(planB);
+
+        REQUIRE(planAsteps == planBsteps);
+    }
+
+
+    SECTION("alternative method of path generation")
+    {
+        std::vector<executor_command_t> planA;
+        std::vector<executor_command_t> planB;
+        configuration_t cfg;
+        cfg.load_defaults();
+        std::cout << cfg << std::endl;
+        {
+            motion_plan_t mp(cfg);
+            mp.gotoxy(distance_t{5.0, 0.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{5.0, -2.0, 0.0, 0.0}, 20.0)
+                .gotoxy(distance_t{5.0, -5.0, 0.0, 0.0}, 5.0);
+            planA = mp.get_motion_plan();
+        }
+
+        {
+            distance_t position_;
+            {
+                std::vector<executor_command_t> fragment;
+                motion_plan_t mp(cfg);
+                mp.set_position(position_).gotoxy(distance_t{5.0, 0.0, 0.0, 0.0}, 20.0);
+                fragment = mp.get_motion_plan();
+                planB.insert(planB.end(), fragment.begin(), fragment.end());
+                position_ = mp.get_position();
+            }
+            {
+                std::vector<executor_command_t> fragment;
+                motion_plan_t mp(cfg);
+                mp.set_position(position_).gotoxy(distance_t{5.0, -2.0, 0.0, 0.0}, 20.0);
+                fragment = mp.get_motion_plan();
+                planB.insert(planB.end(), fragment.begin(), fragment.end());
+                position_ = mp.get_position();
+            }
+            {
+                std::vector<executor_command_t> fragment;
+                motion_plan_t mp(cfg);
+                mp.set_position(position_).gotoxy(distance_t{5.0, -5.0, 0.0, 0.0}, 5.0);
+                fragment = mp.get_motion_plan();
+                planB.insert(planB.end(), fragment.begin(), fragment.end());
+                position_ = mp.get_position();
+            }
+        }
+
+
+        //std::vector<executor_command_t> commands;
+        //steps_t steps = executor_t::commands_to_steps(commands);
+        steps_t planAsteps = executor_t::commands_to_steps(planA);
+        steps_t planBsteps = executor_t::commands_to_steps(planB);
+
+        REQUIRE(planAsteps == planBsteps);
+    }
+}
