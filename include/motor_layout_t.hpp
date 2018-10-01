efaa714b3d63506291b34ce0dcf275df9b4995a0
pantadeusz <tadeusz@puzniakowski.pl>
2018-10-01 13:16:44 +0200

    license update

--- include/motor_layout_t.hpp
+++ include/motor_layout_t.hpp
@@ -1,22 +1,20 @@
 /*
-
     Raspberry Pi G-CODE interpreter
     Copyright (C) 2018  Tadeusz Puźniakowski puzniakowski.pl
-
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
+    it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PUR_steps_from_originE.  See the
-    GNU Affero General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
 #ifndef __RASPIGCD_MOTOR_LAYOUT_T_HPP__
 #define __RASPIGCD_MOTOR_LAYOUT_T_HPP__
 
--- src/motor_layout_t.cpp
+++ src/motor_layout_t.cpp
@@ -1,3 +1,21 @@
+/*
+    Raspberry Pi G-CODE interpreter
+    Copyright (C) 2018  Tadeusz Puźniakowski puzniakowski.pl
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <motor_layout_t.hpp>
 
 #include <configuration_t.hpp>
