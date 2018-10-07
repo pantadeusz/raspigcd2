a9c2abf5698ddbda3e765a859307a4645085f4e8
pantadeusz <pantadeusz@lab3l-18.pjwstk.edu.pl>
2018-10-07 11:51:34 +0200

    experiments

---
+++ app1/src/main/java/pl/edu/pjatk/tau/App.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.tau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
