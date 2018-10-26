35b28bb0a547c699499b9146bda43df6d319619a
pantadeusz <none@none.com>
2018-10-26 20:39:35 +0200

    first moves - longer

--- tddrework/src/raspigcd.cpp
+++ tddrework/src/raspigcd.cpp
@@ -8,13 +8,21 @@ int main() {
     cfg.load_defaults();
     hardware::raspberry_pi_3 raspi3(cfg);
     raspi3.enable_steppers({true});
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < cfg.steppers[2].steps_per_mm*2; i++) {
         hardware::single_step_command cmnd[4];
         cmnd[3].step = 0;
         cmnd[0] = cmnd[1] = cmnd[2] = cmnd[3];
         cmnd[2].step = 1;
         cmnd[2].dir = 1;
         std::this_thread::sleep_for(1ms);
+    }
+    for (int i = 0; i < cfg.steppers[2].steps_per_mm*2; i++) {
+        hardware::single_step_command cmnd[4];
+        cmnd[3].step = 0;
+        cmnd[0] = cmnd[1] = cmnd[2] = cmnd[3];
+        cmnd[2].step = 1;
+        cmnd[2].dir = 0;
+        std::this_thread::sleep_for(6ms);
     }
     raspi3.enable_steppers({false});
     return 0;
