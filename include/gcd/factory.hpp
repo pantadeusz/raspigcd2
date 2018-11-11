608a7d5d306f442009ac1f86065a08f3a7135e1b
pantadeusz <none@none.com>
2018-11-11 08:48:31 +0100

    initial gcode elements factory

---
+++ include/gcd/factory.hpp
@@ -0,0 +1,37 @@
+/*
+    Raspberry Pi G-CODE interpreter
+    Copyright (C) 2018  Tadeusz Puźniakowski puzniakowski.pl
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __RASPIGCD_GCD_FACTORY_T_HPP__
+#define __RASPIGCD_GCD_FACTORY_T_HPP__
+
+namespace raspigcd {
+namespace gcd {
+
+
+class factory
+{
+private:
+public:
+    // todo
+    // generate working set of components for gcd execution
+    // execute gcode as a series of steps
+};
+
+} // namespace hardware
+} // namespace raspigcd
+
+#endif
