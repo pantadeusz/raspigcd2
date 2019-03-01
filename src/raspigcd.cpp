6b26762979b22c865169b24c1773a4713a5d72e8
pantadeusz <tadeusz@puzniakowski.pl>
2019-03-01 14:54:07 +0100

    help screen

--- src/raspigcd.cpp
+++ src/raspigcd.cpp
@@ -166,7 +166,45 @@ int main(int argc, char** argv)
     bool raw_gcode = false; // should I push G commands directly, without adaptation to machine
 
     for (unsigned i = 1; i < args.size(); i++) {
-        if (args.at(i) == "-c") {
+        if ((args.at(i) == "-h") || (args.at(i) == "--help")) {
+            std::cout << "NAME" << std::endl;
+            std::cout << "\t" << args[0] << " - raspigcd runner program." << std::endl;
+            std::cout << "SYNOPSIS" << std::endl;
+            std::cout << "\t" << args[0] << " [options]" << std::endl;
+            std::cout << "DESCRIPTION" << std::endl;
+            std::cout << "\tIt allows for execution of gcode on Raspberry Pi and simulation of such on desktop." << std::endl;
+            std::cout << "\t-c" << std::endl;
+            std::cout << "\t\tprovide configuration file json" << std::endl;
+            std::cout << "\t-f <filename>" << std::endl;
+            std::cout << "\t\tgcode file to execute" << std::endl;
+            std::cout << "\t-h" << std::endl;
+            std::cout << "\t\thelp screen" << std::endl;
+            std::cout << "\t--raw" << std::endl;
+            std::cout << "\t\tTreat the file as raw - no additional processing. No limits check." << std::endl;
+            std::cout << "AUTHOR" << std::endl;
+            std::cout << "\tTadeusz Puźniakowski" << std::endl;
+            std::cout << "REPORTING BUGS" << std::endl;
+            std::cout << "\tOnline at <https://github.com/pantadeusz/raspigcd2>" << std::endl;
+            std::cout << "COPYRIGHT" << std::endl;
+            std::cout << "\t" << std::endl;
+            std::cout << "\tRaspberry Pi G-CODE interpreter" << std::endl;
+            std::cout << "\t" << std::endl;
+            std::cout << "\tCopyright (C) 2019  Tadeusz Puźniakowski puzniakowski.pl" << std::endl;
+            std::cout << "\t" << std::endl;
+            std::cout << "\tThis program is free software: you can redistribute it and/or modify" << std::endl;
+            std::cout << "\tit under the terms of the GNU Affero General Public License as published by" << std::endl;
+            std::cout << "\tthe Free Software Foundation, either version 3 of the License, or" << std::endl;
+            std::cout << "\t(at your option) any later version." << std::endl;
+            std::cout << "\t" << std::endl;
+            std::cout << "\tThis program is distributed in the hope that it will be useful," << std::endl;
+            std::cout << "\tbut WITHOUT ANY WARRANTY; without even the implied warranty of" << std::endl;
+            std::cout << "\tMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" << std::endl;
+            std::cout << "\tGNU Affero General Public License for more details." << std::endl;
+            std::cout << "\t" << std::endl;
+            std::cout << "\tYou should have received a copy of the GNU Affero General Public License" << std::endl;
+            std::cout << "\talong with this program.  If not, see <https://www.gnu.org/licenses/>." << std::endl;
+
+        } else if (args.at(i) == "-c") {
             i++;
             cfg.load(args.at(i));
         } else if (args.at(i) == "--raw") {
