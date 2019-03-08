7d33d7286e66c512644d852b305624ef999b3b5a
pantadeusz <tadeusz@puzniakowski.pl>
2019-03-08 10:01:20 +0100

    removed unnecessary comment

--- src/hardware/stepping.cpp
+++ src/hardware/stepping.cpp
@@ -131,14 +131,16 @@ void stepping_simple_timer::exec(const std::vector<multistep_command>& commands_
     for (const auto& s : commands_to_do) {
         for (int i = 0; i < s.count; i++) {
             if (_terminate_execution > 0) {
-                if (start_counter_delay == 0) {start_counter_delay = _terminate_execution;}
+                if (start_counter_delay == 0) {
+                    start_counter_delay = _terminate_execution;
+                }
                 if (_terminate_execution == 1) {
-                    throw execution_terminated(hardware_commands_to_last_position_after_given_steps(commands_to_do, _tick_index));
+                    throw execution_terminated(
+                        hardware_commands_to_last_position_after_given_steps(commands_to_do, _tick_index)
+                    );
                 } else {
                     _terminate_execution--;
-                    counter_delay = 1000+(start_counter_delay - _terminate_execution);//1000+std::log((start_counter_delay - _terminate_execution))*1000;
-                    //counter_delay = 1000+std::log((start_counter_delay - _terminate_execution))*1000;
-                    //std::cout << "delay counter_delay = " << counter_delay << std::endl;
+                    counter_delay = 1000+(start_counter_delay - _terminate_execution);
                 }
             }
             _steppers_driver->do_step(s.b);
